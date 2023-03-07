73544980eac6b145ccb909f581838b353d303060
my_name <my_name@abc.com>
2023-03-08 04:22:21 +0530

    commit at 4 am march 8

---
+++ script.js
@@ -0,0 +1,83 @@
+const stack = [];
+let count = 0;
+
+const pushBtn = document.getElementById("pushBtn");
+const popBtn = document.getElementById("popBtn");
+
+pushBtn.addEventListener("click", push);
+popBtn.addEventListener("click", pop);
+
+function push() {
+  const numberInput = document.getElementById("numberInput");
+
+  if (numberInput.value === "") {
+    alert("Please enter a number to Push");
+    return;
+  }
+
+  const number = parseInt(numberInput.value);
+
+  stack.push(number);
+  console.log(stack);
+  numberInput.value = "";
+
+  updateStackVisualization();
+}
+
+function pop() {
+  if (stack.length === 0) {
+    alert("Stack is empty!");
+    return;
+  }
+
+  stack.pop();
+  console.log(stack);
+
+  updateStackVisualization();
+}
+
+function updateStackVisualization() {
+  const stackElement = document.getElementById("stack");
+  stackElement.innerHTML = "";
+
+  let j = 0;
+
+  for (let i = stack.length - 1; i >= 0; i--) {
+    const element = document.createElement("div");
+    element.classList.add("stack-element");
+    element.innerHTML = stack[i];
+    element.style.backgroundColor = getRandomColor(j);
+    element.style.opacity = "0.7";
+    stackElement.appendChild(element);
+
+    j++;
+  }
+}
+
+function getRandomColor(j) {
+  const colors = [
+    "#66991A",
+    "#80B300",
+    "#33FFCC",
+    "#6680B3",
+    "#809900",
+    "#FF99E6",
+    "#B34D4D",
+    "#E6B3B3",
+    "#FF1A66",
+    "#CCFF1A",
+    "#66994D",
+    "#E6331A",
+  ];
+  return colors[j % colors.length];
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    push();
+  } else if (event.key === "Delete") {
+    event.preventDefault();
+    pop();
+  }
+});
